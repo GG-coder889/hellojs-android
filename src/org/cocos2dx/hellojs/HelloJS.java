29f5a8b50d5d25f2b99ff02940044508c230c95e
folecr <folecr@gmail.com>
2012-03-26 15:13:11 -0700

    Linear Layout and add a Text View to it

--- src/org/cocos2dx/hellojs/HelloJS.java
+++ src/org/cocos2dx/hellojs/HelloJS.java
@@ -3,6 +3,8 @@ package org.cocos2dx.hellojs;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class HelloJS extends Activity {
@@ -10,25 +12,36 @@ public class HelloJS extends Activity {
 
     /** Called when the activity is first created. */
     @Override
-        public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Log.d(LOGTAG, "onCreate");
 
-        TextView tv = new TextView(this);
+        setContentView(R.layout.mainmenu);
 
+        LinearLayout linearlayout = (LinearLayout) findViewById(R.id.mainmenu);
+
+        addVersionStringsToLinearLayout(linearlayout);
+    }
+
+    static {
+        System.loadLibrary("game");
+    }
+
+    private void addVersionStringsToLinearLayout(LinearLayout linearlayout) {
+        TextView tv = new TextView(this);
+        tv.setText(getLibVersions());
+        linearlayout.addView(tv);
+    }
+
+    private StringBuilder getLibVersions() {
         StringBuilder content = new StringBuilder();
         content.append("Hello Cocos, JS, Android!\n\n");
         content.append("JS VM Version = " + getJSVMVersion() + "\n");
         content.append("Cocos2d Version = " + getCocosVersion() + "\n");
         content.append("Bindings Version = " + getBindingsVersion() + "\n");
 
-        tv.setText(content);
-        setContentView(tv);
-    }
-
-    static {
-        System.loadLibrary("game");
+        return content;
     }
 
     private static native String getJSVMVersion();
