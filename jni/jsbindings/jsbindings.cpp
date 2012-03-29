cba5ae0031b025c1fcb69dec1750937ab16e7af1
folecr <folecr@gmail.com>
2012-03-29 11:06:45 -0700

    Script evalutation!

--- jni/jsbindings/jsbindings.cpp
+++ jni/jsbindings/jsbindings.cpp
@@ -16,7 +16,7 @@ namespace jsbindings {
         return VERSION.c_str();
     }
 
-/* The class of the global object. */
+    // The class of the global object.
     static JSClass global_class =
         {"global",
          JSCLASS_GLOBAL_FLAGS,
@@ -30,8 +30,10 @@ namespace jsbindings {
          JS_FinalizeStub,
          JSCLASS_NO_OPTIONAL_MEMBERS};
 
-    /* The error reporter callback. */
-    void reportError(JSContext *cx, const char *message, JSErrorReport *report) {
+    // The error reporter callback.
+    static void reportError(JSContext *cx,
+                            const char *message,
+                            JSErrorReport *report) {
         LOGD("%s:%u:%s\n",
              report->filename ? report->filename : "<no filename>",
              (unsigned int) report->lineno,
@@ -42,20 +44,15 @@ namespace jsbindings {
         LOGD("runJS");
         // LOGD("script :\n%s", script);
 
-        /* JS variables. */
-        JSRuntime *rt;
-        JSContext *cx;
-        JSObject *global; 
-
         /* Create a JS runtime. */
-        rt = JS_NewRuntime(8L * 1024L * 1024L);
+        JSRuntime *rt = JS_NewRuntime(8L * 1024L * 1024L);
         if (rt == NULL) {
             LOGD("(rt == NULL)");
             return 1;
         }
 
         /* Create a context. */
-        cx = JS_NewContext(rt, 8192);
+        JSContext *cx = JS_NewContext(rt, 8192);
         if (cx == NULL) {
             LOGD("(cx == NULL)");
             return 1;
@@ -63,31 +60,65 @@ namespace jsbindings {
 
         JS_SetOptions(cx, JSOPTION_VAROBJFIX);
         JS_SetVersion(cx, JSVERSION_LATEST);
-        JS_SetErrorReporter(cx, reportError);
+        JS_SetErrorReporter(cx, jsbindings::reportError);
 
         /* Create the global object in a new compartment. */
-        global = JS_NewCompartmentAndGlobalObject(cx, &global_class, NULL);
+        JSObject *global =
+            JS_NewCompartmentAndGlobalObject(cx, &global_class, NULL);
         if (global == NULL) {
             LOGD("(global == NULL)");
             return 1;
         }
 
-        /* Populate the global object with the standard globals, like Object and Array. */
+        // Populate the global object with the standard globals,
+        // like Object and Array.
         if (!JS_InitStandardClasses(cx, global)) {
             LOGD("(!JS_InitStandardClasses(cx, global))");
             return 1;
         }
 
-        // . . .
-        /* Your application code here. This may include JSAPI calls to create your own custom JS objects and run scripts. */
-        //. . .
-        /* Cleanup. */
+        const char *filename = NULL;
+        int lineno = 0;  
+  
+        jsval rval;
+        JSBool evaluatedOK = JS_EvaluateScript(cx, global,
+                                               script, strlen(script),  
+                                               filename, lineno, &rval);  
 
+        if (JS_FALSE == evaluatedOK) {
+            LOGD("evaluatedOK == JS_FALSE)");
+            // return 1;
+        } else {
+            if (JSVAL_IS_NULL(rval)) {
+                LOGD("rval : (JSVAL_IS_NULL(rval)");
+                // return 1;
+            } else if ((JSVAL_IS_BOOLEAN(rval)) &&
+                       (JS_FALSE == (JSVAL_TO_BOOLEAN(rval)))) {
+                LOGD("rval : (return value is JS_FALSE");
+                // return 1;
+            } else if (JSVAL_IS_STRING(rval)) {
+                JSString *str = JS_ValueToString(cx, rval);  
+                if (NULL == str) {
+                    LOGD("rval : return string is NULL");
+                } else {
+                    LOGD("rval : return string =\n%s\n", JS_EncodeString(cx, str));
+                }
+            } else if (JSVAL_IS_NUMBER(rval)) {
+                double number;
+                if (JS_FALSE == JS_ValueToNumber(cx, rval, &number)) {
+                    LOGD("rval : return number could not be converted");
+                } else {
+                    LOGD("rval : return number =\n%f", number);
+                }
+            }
+        }
+  
+        // Cleanup
         JS_DestroyContext(cx);
         JS_DestroyRuntime(rt);
         JS_ShutDown();
 
-        LOGD("runJS done with no reported errors");
+        LOGD("runJS done.");
         return 0;
     }
 
