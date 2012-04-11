594ebb82a83deaf2dcaff79fb7aefa62db6ceb85
folecr <folecr@gmail.com>
2012-04-11 12:58:29 -0700

    Remove cocos dependencies and build options from build scripts and makefiles.

--- jni/Android.mk
+++ jni/Android.mk
@@ -8,9 +8,7 @@ LOCAL_MODULE_FILENAME := libgame
 
 LOCAL_SRC_FILES := helloworld/main.cpp
 
-LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
-LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
-LOCAL_WHOLE_STATIC_LIBRARIES += spidermonkey_static
+LOCAL_WHOLE_STATIC_LIBRARIES := spidermonkey_static
 LOCAL_WHOLE_STATIC_LIBRARIES += jsbindings
 
 LOCAL_LDLIBS += -landroid
@@ -18,6 +16,4 @@ LOCAL_LDLIBS += -landroid
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,jsbindings)
-$(call import-module,cocos2dx)
-$(call import-module,CocosDenshion/android)
 $(call import-module,spidermonkey/android)
--- jni/jsbindings/Android.mk
+++ jni/jsbindings/Android.mk
@@ -11,12 +11,8 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 
-LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
-LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
-LOCAL_WHOLE_STATIC_LIBRARIES += spidermonkey_static
+LOCAL_WHOLE_STATIC_LIBRARIES := spidermonkey_static
 
 include $(BUILD_STATIC_LIBRARY)
 
-$(call import-module,cocos2dx)
-$(call import-module,CocosDenshion/android)
 $(call import-module,spidermonkey/android)
